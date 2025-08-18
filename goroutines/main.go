ef569b8b3403ee07e087d40273f4e40d178f1c98
yadav-kaushal <kaushlendra.yadav@protiviti.co.in>
2025-08-18 11:06:26 +0530

    Learn how to use slices with `go routines` instead of creating `one by one` * use `close` methord to `close channel` * iterate over `channels` using `loop`

--- goroutines/main.go
+++ goroutines/main.go
@@ -6,26 +6,51 @@ import (
 )
 
 
-func greet(name string) {
+func greet(name string, done chan bool) {
 	fmt.Println( "Hello, " + name + "!")
+	done <- true
 }
 func greetUserWithWelcome(name string, doneChan chan bool) {
 	time.Sleep(2*time.Second) // Simulate a delay
 	//return "Welcome, " + name + "!"
 	fmt.Println("Welcome, " + name + "!")
 	doneChan <- true
+	close(doneChan)// Close the channel to signal completion
 }
-func sumOfTwoNumbers(a, b int) {
+func sumOfTwoNumbers(a, b int, done chan bool) {
 	fmt.Println(a+b);
+	done <- true
 }
 
 func main(){
-	greet("kaushal yadav")
-	done := make(chan bool)
-	go greetUserWithWelcome("kaushal yadav", done)
-	<-done // Wait for the goroutine to finish
+	dones := make([]chan bool, 3);
+	dones[0] = make(chan bool)
+	go greet("kaushal yadav", dones[0])
+	dones[1] = make(chan bool)
+	go greetUserWithWelcome("kaushal yadav", dones[1])
+	// Wait for the goroutine to finish
 	fmt.Println("Greeted user in a goroutine")
 	// Call the sumOfTwoNumbers function
-	sumOfTwoNumbers(10,20)
+	dones[2] = make(chan bool)
+	go sumOfTwoNumbers(10,20, dones[2])
+
+	for _, done := range dones{
+		<-done
+		fmt.Println("Done with the goroutine")
+		if done == dones[0] {
+			fmt.Println("Greeted user in main function")
+		} else if done == dones[1] {
+			fmt.Println("Greeted user in a goroutine")
+		} else if done == dones[2] {
+			fmt.Println("Sum of two numbers is calculated in a goroutine")
+		}
+		if done == dones[len(dones)-1] {
+			fmt.Println("All goroutines are done")
+			break
+		}
+		fmt.Println("Waiting for the next goroutine to finish")
+		time.Sleep(1 * time.Second) // Simulate some processing time
+		
+	}
 
 }
