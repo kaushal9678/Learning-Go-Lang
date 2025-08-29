eabc4b568bad852412e15eec1a9621835677b145
yadav-kaushal <kaushlendra.yadav@protiviti.co.in>
2025-08-29 11:18:41 +0530

    *Implemented `urlshortner service` * using `postgres for database service`

--- project-url-shortner/routes/routes.go
+++ project-url-shortner/routes/routes.go
@@ -1,6 +1,16 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"project-url-shortner/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
 func RegisterRoutes(server *gin.Engine) {
-	
+	    // POST route for creating a new short URL
+    server.POST("/shorten", handler.CreateShortURL)
+
+    // GET route for redirecting from a short URL to the original URL
+    // The `:shortUrl` part is a path parameter that Gin will capture.
+    server.GET("/:shortUrl", handler.RedirectToLongURL)
 }
